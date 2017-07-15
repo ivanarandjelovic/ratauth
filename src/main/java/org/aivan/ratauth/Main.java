fd231748ce08239188acda8b19516212938ff24d
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-07-15 18:49:08 +0200

    Fix gradle config

--- src/main/java/org/aivan/ratauth/Main.java
+++ src/main/java/org/aivan/ratauth/Main.java
@@ -1,12 +1,26 @@
 package org.aivan.ratauth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ratpack.server.RatpackServer;
 
 public class Main {
 
-	public static void main(String[] args) {
+	static Logger log = LoggerFactory.getLogger(Main.class);
 
-		System.out.println("Ola from ratauth!");
+	public static void main(String[] args) throws Exception {
 
+		log.info("Ola from ratauth!");
+
+		RatpackServer
+				.start(serverSpec -> serverSpec.handlers(chain -> chain.prefix("api/1", prefix -> prefix.get(ctx -> {
+					log.debug("get 1 start");
+
+					ctx.render("test1");
+				}))));
+
+		log.info("Bye from ratauth!");
 	}
 
 }
