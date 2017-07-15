7aa5fdbb00322a794216f8c7964aebb977b25851
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-07-15 18:59:10 +0200

    refactor handler out of Main class

--- src/main/java/org/aivan/ratauth/Main.java
+++ src/main/java/org/aivan/ratauth/Main.java
@@ -1,5 +1,6 @@
 package org.aivan.ratauth;
 
+import org.aivan.ratauth.handlers.TestHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,16 +10,15 @@ public class Main {
 
 	static Logger log = LoggerFactory.getLogger(Main.class);
 
+	static TestHandler testHandler = new TestHandler();
+	
 	public static void main(String[] args) throws Exception {
 
 		log.info("Ola from ratauth!");
 
 		RatpackServer
-				.start(serverSpec -> serverSpec.handlers(chain -> chain.prefix("api/1", prefix -> prefix.get(ctx -> {
-					log.debug("get 1 start");
-
-					ctx.render("test1");
-				}))));
+				.start(serverSpec -> serverSpec
+						.handlers(chain -> chain.prefix("api/1", prefix -> prefix.get(testHandler))));
 
 		log.info("Bye from ratauth!");
 	}
