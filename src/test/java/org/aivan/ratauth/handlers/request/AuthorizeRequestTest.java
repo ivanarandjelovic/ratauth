7ec01a8d5f03b5a69d6ab963d52aa9d742df9ede
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-07-23 17:59:52 +0200

    Finish unit test for AuthorizeRequest

--- src/test/java/org/aivan/ratauth/handlers/request/AuthorizeRequestTest.java
+++ src/test/java/org/aivan/ratauth/handlers/request/AuthorizeRequestTest.java
@@ -10,14 +10,38 @@ import org.junit.Test;
 public class AuthorizeRequestTest {
 
 	@Test
-	public void responseNoParams() throws Exception {
+	public void defaultValues() throws Exception {
 		Map<String, String> params = new HashMap<String, String>();
-		params.put("client_id","clientId1");
+		AuthorizeRequest request = AuthorizeRequest.loadFrom(params);
+		assertNull(request.getClientId());
+		assertNull(request.getRedirectUri());
+		assertNull(request.getResponseType());
+		assertNull(request.getScope());
+	}
+
+	@Test
+	public void clientId() throws Exception {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("client_id", "clientId1");
 		AuthorizeRequest request = AuthorizeRequest.loadFrom(params);
 		assertEquals("clientId1", request.getClientId());
-		assertNull( request.getRedirectUri());
-		assertNull( request.getResponseType());
-		assertNull( request.getScope());
+		assertNull(request.getRedirectUri());
+		assertNull(request.getResponseType());
+		assertNull(request.getScope());
 	}
-	
+
+	@Test
+	public void allParams() throws Exception {
+		Map<String, String> params = new HashMap<String, String>();
+		params.put("client_id", "clientId1");
+		params.put("redirect_uri", "redirect_uri1");
+		params.put("response_type", "response_type1");
+		params.put("scope", "scope1");
+		AuthorizeRequest request = AuthorizeRequest.loadFrom(params);
+		assertEquals("clientId1", request.getClientId());
+		assertEquals("redirect_uri1", request.getRedirectUri());
+		assertEquals("response_type1", request.getResponseType());
+		assertEquals("scope1", request.getScope());
+	}
+
 }
