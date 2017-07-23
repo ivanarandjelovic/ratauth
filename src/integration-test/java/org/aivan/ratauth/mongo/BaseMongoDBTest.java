845bb4f558bf7366abd755936bcc5aaba9ceb35f
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-07-23 12:48:55 +0200

    Integration test data setup done

--- src/integration-test/java/org/aivan/ratauth/mongo/BaseMongoDBTest.java
+++ src/integration-test/java/org/aivan/ratauth/mongo/BaseMongoDBTest.java
@@ -19,27 +19,33 @@ public class BaseMongoDBTest {
 
 	MongoClient mongoClient;
 	MongoDatabase database;
-	MongoCollection<Document> clientCollection;
+	String[] collectionNames = new String[] { "client", "user" };
 
 	@Before
 	public void setup() throws IOException {
 		// Load the data before the test
 		mongoClient = new MongoClient();
 		database = mongoClient.getDatabase("ratauth");
-		clientCollection = database.getCollection("client");
+		for (String collectionName : collectionNames) {
+			loadCollection(collectionName);
+		}
+	}
+
+	private void loadCollection(String collectionName) throws IOException {
+		MongoCollection<Document> collection = database.getCollection(collectionName);
 
 		boolean clientFileFound = true;
 		int counter = 0;
 		while (clientFileFound) {
 			try (InputStream is = this.getClass().getClassLoader()
-					.getResourceAsStream("data/client" + counter + ".json")) {
+					.getResourceAsStream("data/" + collectionName + counter + ".json")) {
 				if (is == null) {
 					clientFileFound = false;
 				} else {
 					try (Scanner scan = new Scanner(is)) {
 						String json = scan.useDelimiter("\\Z").next();
 						Document client = Document.parse(json);
-						clientCollection.insertOne(client);
+						collection.insertOne(client);
 					}
 					counter++;
 				}
@@ -48,10 +54,17 @@ public class BaseMongoDBTest {
 	}
 
 	@After
-	public void teardown() {
+	public void teardown() throws IOException {
 		// Delete all the data from the database
-		clientCollection.drop();
+		for (String collectionName : collectionNames) {
+			dropCollection(collectionName);
+		}
 		mongoClient.close();
+	}
+
+	private void dropCollection(String collectionName) throws IOException {
+		MongoCollection<Document> collection = database.getCollection(collectionName);
+		collection.drop();
 	}
 
 	@Test
