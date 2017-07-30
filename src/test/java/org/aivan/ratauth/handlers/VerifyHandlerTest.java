15c147e3b9ba49aa16c1748bf52a3aec6dab0215
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2017-07-30 10:19:40 +0200

    Fix unit tests for Async dao

--- src/test/java/org/aivan/ratauth/handlers/VerifyHandlerTest.java
+++ src/test/java/org/aivan/ratauth/handlers/VerifyHandlerTest.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import org.aivan.ratauth.dao.AuthAsyncDao;
 import org.aivan.ratauth.dao.AuthSyncDao;
 import org.aivan.ratauth.domain.Token;
 import org.bson.Document;
@@ -15,6 +16,7 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ratpack.exec.Promise;
 import ratpack.test.handling.HandlingResult;
 import ratpack.test.handling.RequestFixture;
 
@@ -22,12 +24,16 @@ public class VerifyHandlerTest {
 
 	static Logger log = LoggerFactory.getLogger(VerifyHandlerTest.class);
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS");
-	
+
 	@Test
 	public void responseNoParams() throws Exception {
+
+		AuthAsyncDao asyncDao = mock(AuthAsyncDao.class);
+		when(asyncDao.loadToken(any())).thenReturn(Promise.ofNull());
+
 		HandlingResult result = RequestFixture.handle(new VerifyHandler(), fixture -> {
 			fixture.registry(reg -> {
-				reg.add(mock(AuthSyncDao.class));
+				reg.add(asyncDao);
 			});
 			fixture.uri("/verify");
 		});
@@ -36,64 +42,77 @@ public class VerifyHandlerTest {
 
 	@Test
 	public void responseWithParams() throws Exception {
+
+		AuthAsyncDao asyncDao = mock(AuthAsyncDao.class);
+		when(asyncDao.loadToken(any())).thenReturn(Promise.ofNull());
+
 		HandlingResult result = RequestFixture.handle(new VerifyHandler(), fixture -> {
 			fixture.registry(reg -> {
-				reg.add(mock(AuthSyncDao.class));
+				reg.add(asyncDao);
 			});
 			fixture.uri("/verify?token=1");
 		});
 		assertEquals(400, result.getStatus().getCode());
 	}
-	
+
 	@Test
 	public void responseForGoodToken() throws Exception {
+
+		Document doc = new Document();
+		doc.put("_id", new ObjectId("5974dc2babff2d2642f16e41"));
+		doc.put("token", "token");
+		doc.put("expires", new Date(System.currentTimeMillis() + 10000000000L));
+		doc.put("scopes", new ArrayList<String>());
+		doc.put("userId", new ObjectId("5974dc2babff2d2642f16e42"));
+		Token token = new Token(doc);
+
+		AuthAsyncDao asyncDao = mock(AuthAsyncDao.class);
+		when(asyncDao.loadToken(any())).thenReturn(Promise.value(token));
+
 		HandlingResult result = RequestFixture.handle(new VerifyHandler(), fixture -> {
 			fixture.registry(reg -> {
-				AuthSyncDao dao = mock(AuthSyncDao.class);
-				Document doc = new Document();
-				doc.put("_id", new ObjectId("5974dc2babff2d2642f16e41"));
-				doc.put("token", "token");
-				doc.put("expires", new Date(System.currentTimeMillis()+10000000000L));
-				doc.put("scopes", new ArrayList<String>());
-				doc.put("userId", new ObjectId("5974dc2babff2d2642f16e42"));
-				Token token = new Token(doc);
-				when(dao.loadToken(any())).thenReturn(token);
-				reg.add(dao);
+				reg.add(asyncDao);
 			});
 			fixture.uri("/verify?token=whatever");
 		});
 		assertEquals(200, result.getStatus().getCode());
 	}
-	
+
 	@Test
 	public void responseForExpiredToken() throws Exception {
+
+		Document doc = new Document();
+		doc.put("_id", new ObjectId("5974dc2babff2d2642f16e41"));
+		doc.put("token", "token");
+		doc.put("expires", new Date(System.currentTimeMillis() - 1L));
+		doc.put("scopes", new ArrayList<String>());
+		doc.put("userId", new ObjectId("5974dc2babff2d2642f16e42"));
+		Token token = new Token(doc);
+
+		AuthAsyncDao asyncDao = mock(AuthAsyncDao.class);
+		when(asyncDao.loadToken(any())).thenReturn(Promise.value(token));
+
 		HandlingResult result = RequestFixture.handle(new VerifyHandler(), fixture -> {
 			fixture.registry(reg -> {
-				AuthSyncDao dao = mock(AuthSyncDao.class);
-				Document doc = new Document();
-				doc.put("_id", new ObjectId("5974dc2babff2d2642f16e41"));
-				doc.put("token", "token");
-				doc.put("expires", new Date(System.currentTimeMillis()-1L));
-				doc.put("scopes", new ArrayList<String>());
-				doc.put("userId", new ObjectId("5974dc2babff2d2642f16e42"));
-				Token token = new Token(doc);
-				when(dao.loadToken(any())).thenReturn(token);
-				reg.add(dao);
+				reg.add(asyncDao);
 			});
 			fixture.uri("/verify?token=whatever");
 		});
 		assertEquals(400, result.getStatus().getCode());
 	}
-	
+
 	@Test
 	public void responseForEmptyToken() throws Exception {
+
+		Document doc = new Document();
+		Token token = new Token(doc);
+
+		AuthAsyncDao asyncDao = mock(AuthAsyncDao.class);
+		when(asyncDao.loadToken(any())).thenReturn(Promise.value(token));
+
 		HandlingResult result = RequestFixture.handle(new VerifyHandler(), fixture -> {
 			fixture.registry(reg -> {
-				AuthSyncDao dao = mock(AuthSyncDao.class);
-				Document doc = new Document();
-				Token token = new Token(doc);
-				when(dao.loadToken(any())).thenReturn(token);
-				reg.add(dao);
+				reg.add(asyncDao);
 			});
 			fixture.uri("/verify?token=whatever");
 		});
